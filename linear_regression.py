9fd576d6776529ea524d53f373db63b286553d6d
nahiduddin007 <Nahiduddin080898>
2019-02-28 01:53:16 +0600

    My First Commit

---
+++ linear_regression.py
@@ -0,0 +1,35 @@
+
+# coding: utf-8
+
+# In[3]:
+
+
+import os
+import pandas as pd
+import matplotlib.pyplot as plt
+
+#importing dataset
+dataset=pd.read_csv('Beton.csv')
+X=dataset.iloc[:,:-1].values
+y=dataset.iloc[:,1].values
+
+# Spliting the dataset into the tranning set and Test set
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size =1/3,random_state=0)
+
+
+#Fitting sample linear Regression to Tranning set
+from sklearn.linear_model import LinearRegression
+regressor=LinearRegression()
+regressor=regressor.fit(X_train, y_train)
+
+
+#Predicting the Test Result
+
+y_pred=regressor.predict(X_test)
+
+print(y_pred)
+
+
+
+
